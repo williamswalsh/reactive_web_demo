1fee8c3bb0d9f8d1dd48eb6b890cb1baa32806e5
william walsh <william.walsh@digitary.net>
2022-01-19 08:16:59 +0000

    Add test to compare the response body JSON with a file.

--- src/test/java/ie/williamswalsh/reactive_web_demo/SimpleTests.java
+++ src/test/java/ie/williamswalsh/reactive_web_demo/SimpleTests.java
@@ -3,7 +3,12 @@ package ie.williamswalsh.reactive_web_demo;
 import ie.williamswalsh.reactive_web_demo.web.OtherCtrlr;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 import static org.mockito.Mockito.when;
 
@@ -39,11 +44,25 @@ class SimpleTests {
                 .jsonPath("non_existant_field").doesNotExist();
 
     }
-}
 
-class Sample {
+    @Test
+    void testingResponseBodyWithJsonFile() throws IOException {
+        // Create WebTestClient from ctrlr
+        WebTestClient webTestClient = WebTestClient.bindToController(new OtherCtrlr()).build();
 
-    public String returnString() {
-        return "x";
+        // Access JSON file
+        ClassPathResource classPathResource = new ClassPathResource("method2.json");
+        String method2ResponseBody = StreamUtils.copyToString(classPathResource.getInputStream(), Charset.defaultCharset());
+
+
+        // Expectations/Assertions
+        webTestClient
+                .get()
+                .uri("/getCar")
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .json(method2ResponseBody);
+
     }
 }
